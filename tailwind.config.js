5879701fa7dd0d7d499db80d27e87ceee85cc793
ursachecodrut <ursache.codrut71@gmail.com>
2021-10-11 11:53:51 +0300

    setup

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+	purge: ['./src/*/.{js,jsx,ts,tsx}', './public/index.html'],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			colors: {
+				background: '#282a36',
+				currentLine: '#44475a',
+				foreground: '#f8f8f2',
+				comment: '#6272a4',
+				cyan: '#8be9fd',
+				green: '#50fa7b',
+				orange: '#ffb86c',
+				pink: '#ff79c6',
+				purple: '#bd93f9',
+				red: '#ff5555',
+				yellow: '#f1fa8c',
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
